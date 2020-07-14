21509e53e038bdabb2780206848045142f278119
Devmadia <agathaphan@icloud.com>
2020-07-14 17:06:59 -0500

    added module.export capabilities

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// receiving module.exports from page-template.js
+const generatePage = require('./src/page-template.js');
+
 // in order to use File System, the below constant is necessary
 const fs = require('fs');
 
@@ -5,18 +8,17 @@ const fs = require('fs');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 // array index
-const name = profileDataArgs[0];
-const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-        console.log('================');
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//         console.log('================');
 
-        profileDataArr.forEach(profileItem => console.log(profileItem));
+//         profileDataArr.forEach(profileItem => console.log(profileItem));
 
-    }
-};
+//     }
+// };
 
 // const generatePage = () => 'Name: Jane, Github: janehub';
 
@@ -31,25 +33,7 @@ const printProfileData = profileDataArr => {
 //     `;
 //   };
 
-// change the generatePage() function in app.js to output the following HTML string instead
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
-  
-    <body>
-      <h1>${name}</h1>
-      <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-};
+
 
 // wrap the string in backticks and interpolate the variables with the ${<variable>} syntax
 // console.log(generatePage('Jane', 'janehub'));
@@ -62,10 +46,10 @@ const generatePage = (name, github) => {
 
 
 //  callback function block with conditional statement to check for err
-fs.writeFile('index.html', generatePage(name, github), err => {
+fs.writeFile('./index.html', generatePage(name, github), err => {
 
     // if an error exists, it'll throw an error message out
-    if (err) throw err;
+    if (err) throw new Error(err);
   
     console.log('Portfolio complete! Check out index.html to see the output!');
 });
